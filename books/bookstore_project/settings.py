5097d569ae2dc3bd8e6f850caf264cc2336549d5
jion-kozono <zeon66190329@gmail.com>
2019-12-04 01:28:48 +0900

    heroku push 2019-12-04

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -208,6 +208,7 @@ if ENVIRONMENT == 'production':
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
+# Heroku
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
